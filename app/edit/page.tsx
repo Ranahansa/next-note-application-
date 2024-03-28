069a3f15e17b3c123da09631cb0fb5ca22acfe3e
Ranahansa <CODSE213F-053@student.nibm.lk>
2024-03-28 15:37:06 +0530

    Add page component for editing notes

---
+++ app/edit/page.tsx
@@ -0,0 +1,53 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { IoCaretBack } from "react-icons/io5";
+import { MdEditCalendar } from "react-icons/md";
+
+const page = () => {
+    return (
+        <div>
+            <Link href="/">
+                <button className="bg-[#64a0aa] hover: bg-[#64a0aa]/90 text-white font-bold rounded-lg text-sm px-3 py-2 text-center inline-flex dark:focus:ring-[#64a0aa]/50 mt-4 sm:mt-0 ml-20 ">
+                    <IoCaretBack className="w-4 h-4 mt-[0.15rem] text-black" />
+                    GoBack
+                </button>
+            </Link>
+            <div className="max-w-5xl mx-auto pt-5">
+                {/* Container for the title and form */}
+                <span className="text-2xl sm:text-3xl font-extrabold flex pt-6">
+                    Edit your note
+                    <MdEditCalendar className="ml-2" />
+                </span>
+                <form className="flex flex-col gap-9 text-black mb-10 ml-10">
+                    <input
+                        type="text"
+                        name="title"
+                        placeholder="Title"
+                        className="py-2 px-4 mt-10 w-3/4 rounded-md"
+                    />
+
+                    <textarea
+                        name="description"
+                        placeholder="Description"
+                        className="py-1 px-4 w-3/4 rounded-md resize-none h-60 overflow-y-auto"
+                    ></textarea>
+                    <button className="bg-[#64a0aa] hover: bg-[#64a0aa]/90 text-white font-bold rounded-lg text-sm px-5 py-2 text-center inline-flex dark:focus:ring-[#64a0aa]/50 mt-4 sm:mt-0 w-1/6 justify-center">
+                        Save Note
+                    </button>
+                </form>
+                <div className="absolute bottom-[-15.75rem] right-1 ">
+                    <Image
+                        src="/edit.png"
+                        alt="note"
+                        width={520}
+                        height={520}
+                        className="object-contain"
+                    />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default page;
